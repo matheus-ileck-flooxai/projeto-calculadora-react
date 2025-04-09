1fc06323ba426e9677988953d6b6a635f1dc9886
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-09 13:03:10 -0300

    feat: add logic to set button classes

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,32 +5,49 @@ import Display from "../components/Display";
 
 export default class Calculator extends Component{
 
+
+    clearMemory(){
+
+        console.log("limpar")
+    }
+
+    setOperation(operation){
+        console.log(operation);
+        
+        
+    }
+
+    addDigit(n){
+        console.log(n);
+        
+    }
+
+
     render(){
 
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
         return (
 
             <div className="calculator">
-                <Display value={100}></Display>
-                <Button label="AC"></Button>
-                <Button label="/"></Button>
-                <Button label="7"></Button>
-                <Button label="8"></Button>
-                <Button label="9"></Button>
-                <Button label="*"></Button>
-                <Button label="4"></Button>
-                <Button label="5"></Button>
-                <Button label="6"></Button>
-                <Button label="-"></Button>
-                <Button label="1"></Button>
-                <Button label="2"></Button>
-                <Button label="3"></Button>
-                <Button label="+"></Button>
-                <Button label="0"></Button>
-                <Button label="."></Button>
-                <Button label="="></Button>
-           
-              
-
+                <Display value={100} ></Display>
+                <Button label="AC" click={() => this.clearMemory()} triple></Button>
+                <Button label="/" click={setOperation} operation></Button>
+                <Button label="7" click={addDigit}></Button>
+                <Button label="8" click={addDigit}></Button>
+                <Button label="9" click={addDigit}></Button>
+                <Button label="*" click={setOperation} operation></Button>
+                <Button label="4" click={addDigit}></Button>
+                <Button label="5" click={addDigit}></Button>
+                <Button label="6" click={addDigit}></Button>
+                <Button label="-" click={setOperation} operation></Button>
+                <Button label="1" click={addDigit}></Button>
+                <Button label="2" click={addDigit}></Button>
+                <Button label="3" click={addDigit}></Button>
+                <Button label="+" click={setOperation} operation></Button>
+                <Button label="0" click={addDigit} double></Button>
+                <Button label="." click={addDigit}></Button>
+                <Button label="=" click={setOperation} operation></Button>
             </div>
 
         )
